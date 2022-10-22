e3a3e67d9ac4cc756110b2a7d7f9a10a9b4d800b
Martinsanchez06 <sanchezmartin2435gmail.com>
2022-10-22 14:51:04 -0500

    Cambios para pull

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const db = require('../database/models')
+const bcryptjs = require('bcryptjs');
 
 const userController = {
 
@@ -7,11 +8,20 @@ const userController = {
         db.gerencias.findAll()
         .then(function(gerencias){
             res.render("register", {gerencias})
+            console.log(gerencias);
         })
     },
     register: (req, res) => {
-       
-        return console.log(hola);
+        try {
+            db.usuarios.create({
+                ...req.body,
+                contrasenia: bcryptjs.hashSync(req.body.password, 10),
+                rolUsuario: 2
+            })
+            res.status(200).redirect('/users/login')
+        } catch (error) {
+            res.status(400).send('Usuario no creado')
+        }
     },
     formLogin: (req, res) => {
         res.render("login");
