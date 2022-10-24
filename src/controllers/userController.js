1fdd976043cda59fe1a86223bc9e390db86d5efa
Martinsanchez06 <sanchezmartin2435gmail.com>
2022-10-24 15:46:31 -0500

    Agregando logica de aleatorio

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,44 +6,44 @@ const userController = {
 
     formRegister: (req, res) => {
         db.gerencias.findAll()
-        .then(function(gerencias){
-            res.render("register", {gerencias})
-        })
+            .then(function (gerencias) {
+                res.render("register", { gerencias })
+            })
     },
     register: (req, res) => {
-        console.log(req.body);  
+        console.log(req.body);
         db.usuarios.findAll()
-         .then(function(usuarios){
-            console.log(usuarios);
-            if(usuarios.length > 0){
-                try {
-                    db.usuarios.create({
-                       ...req.body,
-                       contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10), 
-                       // MODIFICAR: eliminar usuarios en db para registrar un usuario admin y otro de cliente
-                       rolUsuario: 2
-                   })
-                   res.status(200).redirect('/users/login')
-               } catch (error) {
-                   console.log(error);
-               }
-            } else {
-                try {
-                    db.usuarios.create({
-                       ...req.body,
-                       contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10), 
-                       // MODIFICAR: eliminar usuarios en db para registrar un usuario admin y otro de cliente
-                       rolUsuario: 1
-                   })
-                   res.status(200).redirect('/users/login')
-               } catch (error) {
-                   console.log(error);
-               }
-            }
-         })
-        
-           
-        
+            .then(function (usuarios) {
+                console.log(usuarios);
+                if (usuarios.length > 0) {
+                    try {
+                        db.usuarios.create({
+                            ...req.body,
+                            contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
+                            // MODIFICAR: eliminar usuarios en db para registrar un usuario admin y otro de cliente
+                            rolUsuario: 2
+                        })
+                        res.status(200).redirect('/users/login')
+                    } catch (error) {
+                        console.log(error);
+                    }
+                } else {
+                    try {
+                        db.usuarios.create({
+                            ...req.body,
+                            contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
+                            // MODIFICAR: eliminar usuarios en db para registrar un usuario admin y otro de cliente
+                            rolUsuario: 1
+                        })
+                        res.status(200).redirect('/users/login')
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            })
+
+
+
     },
     formLogin: (req, res) => {
         res.render("login");
@@ -51,43 +51,41 @@ const userController = {
     login: (req, res) => {
         try {
             db.usuarios.findOne({ // ERROR: No se estaba llamando bien el modelo es "usuarios" y decia "Usuario"
-                where : { 
-                    email : req.body.email
+                where: {
+                    email: req.body.email
                 }
             })
-            .then(function (userLog){
-                
-                if (userLog) {
-                    let contraseñaCorrecta = bcryptjs.compareSync(req.body.contrasenia, userLog.contrasenia)
-                    if (contraseñaCorrecta) {
-                        // req.session.usuarioLogueado = userLog;
-                        return res.render('prueba', { userLog });
-                    }
-                }
-                return res.render('login', {
-                    errors: {
-                        email: {
-                            msg: 'Usuario no valido'
+                .then(function (userLog) {
+
+                    if (userLog) {
+                        let contraseñaCorrecta = bcryptjs.compareSync(req.body.contrasenia, userLog.contrasenia)
+                        if (contraseñaCorrecta) {
+                            // req.session.usuarioLogueado = userLog;
+                            return res.render('prueba', { userLog });
                         }
                     }
+                    return res.render('login', {
+                        errors: {
+                            email: {
+                                msg: 'Usuario no valido'
+                            }
+                        }
+                    })
                 })
-            })   
         } catch (error) {
             console.log(error);
         }
     },
     perfil: (req, res) => {
-        db.usuarios.findOne({
-            where: { id: req.params.id },
-        })
-        .then(function (usuarios) {
-            
-            res.render("perfil", {
-                usuario: usuarios
+        let listaUsuarios = db.usuarios.findAll()
+        let usuarioEncontrado = db.usuarios.findOne({ where: { id: req.params.id } })
+        Promise.all([listaUsuarios, usuarioEncontrado])
+            .then(function ([usuarios, usuario]) {
+                const aleatorio1 = usuarios[Math.floor(Math.random() * usuarios.length)];
+                console.log(aleatorio1);
+                return res.render('perfil', { usuario, aleatorio1 });
             })
-        })
     }
-
 };
 
 module.exports = userController;
