1e3b181a3d4f7b0c94222eef17c3d1d041afd0be
Applepie46 <emmanuel.cadavid46@gmail.com>
2022-10-27 10:19:58 -0500

    Agregando estilos a un botton de perfil

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,30 +1,30 @@
 //AdminRoot
 
-// module.exports = {
-//   "development": {
-//     "username": "root",
-//     "password": "",
-//     "database": "codigomoda_db",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": "",
-//     "database": "codigomoda_db",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": "",
-//     "database": "codigomoda_db",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
+ module.exports = {
+   "development": {
+     "username": "root",
+     "password": "",
+     "database": "codigomoda_db",
+     "host": "127.0.0.1",
+     "dialect": "mysql"
+   },
+   "test": {
+     "username": "root",
+     "password": "",
+     "database": "codigomoda_db",
+     "host": "127.0.0.1",
+     "dialect": "mysql"
+   },
+   "production": {
+     "username": "root",
+     "password": "",
+     "database": "codigomoda_db",
+     "host": "127.0.0.1",
+     "dialect": "mysql"
+   }
+ }
 
-module.exports = {
+/*module.exports = {
   "development": {
     "username": "root",
     "password": "AdminRoot",
@@ -46,4 +46,4 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+}*/
